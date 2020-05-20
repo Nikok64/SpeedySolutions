b870a17f57eee716e2e485e5c0e2cf8844c323bd
Tradernfl676 <65611080+Tradernfl676@users.noreply.github.com>
2020-05-20 19:15:25 -0400

    basework testing

---
+++ raceCarGame.js
@@ -0,0 +1,13 @@
+
+function desertFunction(){
+  document.getElementById("topWall").style.backgroundImage="url('desert.png')";
+}
+function cityFunction(){
+    document.getElementById("topWall").style.backgroundImage="url('Citything.png')";
+}
+function forestFunction(){
+    document.getElementById("topWall").style.backgroundImage="url('forest.png')";
+}
+function farmFunction(){
+      document.getElementById("topWall").style.backgroundImage="url('farm (5).png')";
+}
